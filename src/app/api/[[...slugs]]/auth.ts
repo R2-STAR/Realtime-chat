b2864499d8a123302bd9fe0037460ffd2b45823a
R2-STAR <ridhimab1947@gmail.com>
2026-01-22 11:46:40 +0000

    updates

--- src/app/api/[[...slugs]]/auth.ts
+++ src/app/api/[[...slugs]]/auth.ts
@@ -8,25 +8,28 @@ class AuthError extends Error {
     }
 }
 
-export const authMiddleware = new Elysia({ name: "auth" }).error({ AuthError }).onError(({ code, set }) => { //whenever an AuthError is thrown anywhere in this app, this middleware will catch it.
-    if(code === "AuthError") {
-        set.status = 401
-        return { error: "Unauthorised" }
-    }
-}).derive({ as: "scoped" }, async ({ query, cookie }) => {
-    const roomId = query.roomId //which room is user trying to connect to 
-    const token = cookie["x-auth-token"].value as string | undefined //is the token allowing them or not
+export const authMiddleware = new Elysia({ name: "auth" })
+    .error({ AuthError })
+    .onError(({ code, set }) => { //whenever an AuthError is thrown anywhere in this app, this middleware will catch it.
+        if(code === "AuthError") {
+            set.status = 401
+            return { error: "Unauthorised" }
+        }
+    })
+    .derive({ as: "scoped" }, async ({ query, cookie }) => {
+        const roomId = query.roomId //which room is user trying to connect to 
+        const token = cookie["x-auth-token"].value as string | undefined //is the token allowing them or not
 
 
-    if(!roomId || !token) {
-        throw new AuthError("Missing roomId or token.")
-    }
+        if(!roomId || !token) {
+            throw new AuthError("Missing roomId or token.")
+        }
 
-    const connected = await redis.hget<string[]>(`meta:${roomId}`, "connected") //metadate of the room user is trynna connect to
+        const connected = await redis.hget<string[]>(`meta:${roomId}`, "connected") //metadate of the room user is trynna connect to
 
-    if(!connected?.includes(token)) {
-        throw new AuthError("Invalid token")
-    }
+        if(!connected?.includes(token)) {
+            throw new AuthError("Invalid token")
+        }
 
-    return { auth: { roomId, token, connected }}
-})
+        return { auth: { roomId, token, connected }}
+    })
--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -8,72 +8,88 @@ import { Message, realtime } from '@/lib/realtime'
 
 const ROOM_TTL_SECONDS = 60 * 10    // 10 minutes
 
-const rooms = new Elysia({ prefix: "/room" }).post("/create", async () => {
-    const roomId = nanoid()
+const rooms = new Elysia({ prefix: "/room" })
+    .post("/create", async () => {
+        const roomId = nanoid()
 
-    await redis.hset(`meta:${roomId}`, {
-        connected: [], //which users are currently connected to the chat room
-        createdAt: Date.now(),
+        await redis.hset(`meta:${roomId}`, {
+            connected: [], //which users are currently connected to the chat room
+            createdAt: Date.now(),
+        })
+
+        await redis.expire(`meta:${roomId}`, ROOM_TTL_SECONDS)
+
+        return { roomId }
     })
-
-    await redis.expire(`meta:${roomId}`, ROOM_TTL_SECONDS)
-
-    return { roomId }
-}).use(authMiddleware).get("/ttl", async ({ auth }) => {
-    const ttl = await redis.ttl(`meta:${auth.roomId}`)
-    return { ttl: ttl > 0 ? ttl : 0 }//pass the ttl to the frontend
-}, { query: z.object({ roomId: z.string() }) }
- ).delete("/", async ({ auth }) => {
-    await realtime.channel(auth.roomId).emit("chat.destroy", { isDestroyed: true})  //broadcasts a “room closed” event to all connected users instantly.
+    .use(authMiddleware)
+    .get(
+        "/ttl", 
+        async ({ auth }) => {
+            const ttl = await redis.ttl(`meta:${auth.roomId}`)
+            return { ttl: ttl > 0 ? ttl : 0 }//pass the ttl to the frontend
+        }, 
+        { query: z.object({ roomId: z.string() }) }
+    )
+    .delete(
+        "/", 
+        async ({ auth }) => {
+            await realtime.channel(auth.roomId).emit("chat.destroy", { isDestroyed: true})  //broadcasts a “room closed” event to all connected users instantly.
     
-    await Promise.all([       //Deletes everything in parallel (faster)
-        redis.del(auth.roomId),
-        redis.del(`meta:${auth.roomId}`),
-        redis.del(`messages:${auth.roomId}`),
-    ]) 
-    
-}, {query: z.object({roomId: z.string() })} ) 
+            await Promise.all([       //Deletes everything in parallel (faster)
+                redis.del(auth.roomId),
+                redis.del(`meta:${auth.roomId}`),
+                redis.del(`messages:${auth.roomId}`),
+            ]) 
+        }, 
+        {query: z.object({roomId: z.string() })} 
+    ) 
 
 //any API route we define on this endpoint will automatically use the authMiddleware before executing its logic
-const messages = new Elysia({ prefix: "/messages" }).use(authMiddleware).post("/", async ({ body, auth }) => {
-    const { sender, text } = body
-    const { roomId } = auth
-    const roomExists = await redis.exists(`meta: ${roomId}`)
+const messages = new Elysia({ prefix: "/messages" })
+    .use(authMiddleware)
+    .post(
+        "/", 
+        async ({ body, auth }) => {
+            const { sender, text } = body
+            const { roomId } = auth
+            const roomExists = await redis.exists(`meta: ${roomId}`)
 
-    if(!roomExists) {
-        throw new Error("Room does not exist")
-    }
+            if(!roomExists) {
+                throw new Error("Room does not exist")
+            }
 
-    const message: Message = {
-        id: nanoid(),
-        sender,
-        text,
-        timestamp: Date.now(),
-        roomId,
-    }
+            const message: Message = {
+                id: nanoid(),
+                sender,
+                text,
+                timestamp: Date.now(),
+                roomId,
+            }
 
-    //messages history from the backend
-    await redis.rpush(`messages:${roomId}`, {...message, token:auth.token})
+            //messages history from the backend
+            await redis.rpush(`messages:${roomId}`, {...message, token:auth.token})
 
-    //so that every client in this room receives the message
-    await realtime.channel(roomId).emit("chat.message", message)
+            //so that every client in this room receives the message
+            await realtime.channel(roomId).emit("chat.message", message)
 
-    //housekeeping
-    const remaining = await redis.ttl(`meta:${roomId}`)
+            //housekeeping
+            const remaining = await redis.ttl(`meta:${roomId}`)
 
-    await Promise.all([
-        redis.expire(`messages:${roomId}`, remaining),
-        redis.expire(`history:${roomId}`, remaining),
-        redis.expire(roomId, remaining)
-    ])
-}, {
-    query: z.object({ roomId: z.string() }),
-    body: z.object({
+            await Promise.all([
+                redis.expire(`messages:${roomId}`, remaining),
+                redis.expire(`history:${roomId}`, remaining),
+                redis.expire(roomId, remaining)
+            ])
+        }, 
+        {
+        query: z.object({ roomId: z.string() }),
+        body: z.object({
             sender: z.string().max(100),
             text: z.string().max(1000),
         }),
     }
-).get("/", async ({ auth }) => {
+)
+.get("/", async ({ auth }) => {
     const messages = await redis.lrange<Message>(`messages:${auth.roomId}`, 0, -1) //i.e, no end, give us all messages
 
     return {
